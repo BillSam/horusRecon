ff1e11e31cd7d8e2a8f94deaddf1ebf5dd7a4e7f
mbugua <sam.wangui13@gmail.com>
2020-04-26 14:31:37 +0300

    fix

--- lazyrecon.sh
+++ lazyrecon.sh
@@ -89,6 +89,7 @@ ffuffingback(){
     gau $dom > ./$domain/$foldername/gau.tmp
     ffuf -mc all -c -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u FUZZ -w ./$domain/$foldername/gau.tmp -o ./$domain/$foldername/result_gau.tmp
     cat ./$domain/$foldername/result_gau.tmp | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' >> ./$domain/$foldername/result_wayback.txt
+    cat ./$domain/$foldername/result_gau.tmp | jq '[.results[]|{url: .url}]' | grep -oP "url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{"$6}' | sed 's/\"//g' >> ./$domain/$foldername/rresult_wayback.txt
     rm ./$domain/$foldername/result_gau.tmp
     rm ./$domain/$foldername/gau.tmp
     printf "\nDone. Result is stored in result_wayback.txt\n"
