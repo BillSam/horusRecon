4195319b416fd9d3cae44d2135cf459e298fce79
mbugua <sam.wangui13@gmail.com>
2020-04-28 09:31:06 +0300

    added subover

--- lazyrecon.sh
+++ lazyrecon.sh
@@ -347,9 +347,9 @@ nsrecords(){
 
                 cat ./$domain/$foldername/temp.txt | awk '{print $3}' | sort -u | while read line; do
                 echo "Looking for subovers...."
-                SubOver -l ./$domain/$foldername/temp.txt -timeout 5 -o ./$domain/$foldername/subover.out;
+                SubOver -l ./$domain/$foldername/temp.txt -timeout 5 -o ./$domain/$foldername/subover.out
                 echo "Done with SubOver"
-                
+
                 cat ./$domain/$foldername/temp.txt | awk '{print $3}' | sort -u | while read line; do
                 wildcard=$(cat ./$domain/$foldername/temp.txt | grep -m 1 $line)
                 echo "$wildcard" >> ./$domain/$foldername/cleantemp.txt
